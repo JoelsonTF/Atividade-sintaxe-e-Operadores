075dc608abc85dbe7e954933e59e030b9ebc6dd6
Joelson <joelsontorres.f@gmail.com>
2022-07-08 14:08:31 -0300

    first commit

---
+++ Unititled.js
@@ -0,0 +1,13 @@
+function compararNumeros(num1,num2) {
+    const saoIguais = num1 === num2
+    const soma = num1 + num2
+
+     return saoIguais ? "são iguais" : " Não são iguais"
+
+    // if(saoIguais) {
+    //     return "São iguais"
+    // }
+
+    // return "Não são iguais"
+
+}
